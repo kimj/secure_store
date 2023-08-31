9e87773804f2859a166eb7c93410c0762f3694d6
kimj <jinn.kim@gmail.com>
2023-08-30 20:47:34 -0400

    adding tests and mock secure_store

--- test/secure_store_method_channel_test.dart
+++ test/secure_store_method_channel_test.dart
@@ -8,20 +8,60 @@ void main() {
   SecureStore platform = SecureStore();
   const MethodChannel channel = MethodChannel('secure_store');
 
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
+  group('secure_store_method_tests', () {
+    TestWidgetsFlutterBinding.ensureInitialized();
+
+    handler(MethodCall methodCall) async {
+      switch (methodCall.method) {
+        case 'read':
+          {
+            return null;
+          }
+        case 'write':
+          {}
+        case 'contains':
+          {}
+        case 'delete':
+          {
+            return null;
+          }
+      }
+    }
+
+    // Mainly useful in internal tests of
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, handler);
+
+    tearDown(() {});
+
+    var key = 'testKey';
+    var value = 'testValue';
+
+    test('contains', () async {
+      var result = await platform.contains(key: key);
+      expect(result, true);
+    });
+
+    test('read', () async {
+      var result = await platform.read(key: 'testKey');
+      expect(result, 'testValue');
+    });
+
+    test('delete', () async {
+      var result = await platform.contains(key: key);
+      expect(result, true);
+    });
+
+    test('write', () async {
+      await platform.write(key: key, value: value);
+
+      var result = await platform.read(key: 'testKey');
+      expect(result, 'testValue');
+    });
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
-
-  /*test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });*/
 }
--- test/secure_store_test.dart
+++ test/secure_store_test.dart
@@ -1,52 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:secure_store/secure_store_platform_interface.dart';
 import 'package:secure_store/secure_store_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockSecureStorePlatform
-    with MockPlatformInterfaceMixin
-    implements SecureStorePlatform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-
-  @override
-  Future<void> contains({required String key}) {
-    // TODO: implement contains
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> delete({required String key}) {
-    // TODO: implement delete
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<String> read({required String key}) {
-    // TODO: implement read
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<void> write({required String key, required String value}) {
-    // TODO: implement write
-    throw UnimplementedError();
-  }
-}
+import 'package:secure_store/secure_store_platform_interface.dart';
 
 void main() {
   final SecureStorePlatform initialPlatform = SecureStorePlatform.instance;
 
-  test('$SecureStore is the default instance', () {
-    expect(initialPlatform, isInstanceOf<SecureStore>());
-  });
+  setUpAll(() => {});
 
-  /*test('getPlatformVersion', () async {
+  test('multiple operations ', () async {
     SecureStore secureStorePlugin = SecureStore();
-    MockSecureStorePlatform fakePlatform = MockSecureStorePlatform();
-    SecureStorePlatform.instance = fakePlatform;
 
-    expect(await secureStorePlugin.getPlatformVersion(), '42');
-  });*/
+    await secureStorePlugin.write(key: 'testKey', value: 'testValue');
+    await secureStorePlugin.write(key: 'testKey1', value: 'testValue1');
+
+    expect(await secureStorePlugin.read(key: 'testKey1'), 'testValue1');
+    expect(await secureStorePlugin.contains(key: 'testKey'), true);
+  });
 }
